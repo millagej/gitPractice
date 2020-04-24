5bba28a34a75a0968b98108d1c942e7d25f13401
joshua.c.millage <millagej@icloud.com>
2020-04-24 14:33:11 -0400

    add gitFriday.java

---
+++ src/Test/gitFriday.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class gitFriday {
+    public static void main(String[] args) {
+        System.out.println('z' == 100); //false
+        System.out.println('z' == 122); //true
+        System.out.println('a' == 'A'); // false
+        System.out.println('a' > 'b'); //false
+        System.out.println('a' != (98-1)); //false
+
+        boolean r1 = 'a' > 50; //true
+        boolean r2 = 'b' == 'a'+1;//true
+
+    }
+}
