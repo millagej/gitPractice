a7bdd3268761fdee1a753571f33a0818ecc60385
joshua.c.millage <millagej@icloud.com>
2020-04-28 15:57:01 -0400

    fixed the code

--- src/Test/HappyBirthDay.java
+++ src/Test/HappyBirthDay.java
@@ -4,6 +4,6 @@ public class HappyBirthDay {
     public static void main(String[] args) {
         System.out.println("my birthday is comming up soon \"Hooray\"");
         System.out.println("\t\t\t\tmy fovorite book is the \"Dark Tower Series\"\n \"Lord of the Rings\"");
-        System.out.println("Just saying Hi!";
+        System.out.println("Just saying Hi!");
     }
 }
